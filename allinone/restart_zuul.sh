4049c8f627267b5b4073e02cdb275cddaa17b1fb
liusheng <liusheng@huawei.com>
2018-01-19 01:12:23 +0000

    add a script for restarting zuul services

---
+++ allinone/restart_zuul.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ ! -d "/var/run/zuul/" ]; then
+    mkdir -p /var/run/zuul/
+fi
+
+for bin in "executor" "merger" "scheduler" "web"
+do
+    for pid in `ps -ef |grep /usr/local/bin/zuul-$bin |grep -v grep | awk '{print $2}'`
+    do
+        kill -9 $pid
+    done
+    rm /var/run/zuul/$bin.pid -fr
+done
+
+sleep 3
+
+for bin in "executor" "merger" "scheduler" "web"
+do
+    
+    rm /var/run/zuul/$bin.pid -fr
+    if [ "$bin" = "executor" ]; then
+        /usr/bin/python3 /usr/local/bin/zuul-$bin --keep-jobdir
+        continue
+    fi
+    if [ "$bin" = "web" ]; then
+        sleep 5
+    fi
+    /usr/bin/python3 /usr/local/bin/zuul-$bin
+    sleep 2
+done
+
+
